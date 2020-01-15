602af3216691c2549a02464ceebc04458411bb32
sven-zo <svenzo@outlook.com>
2020-01-15 22:00:30 +0100

    :sparkles: add hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hero">
+    <div class="h-full flex flex-col justify-center w-1/2">
+      <h1>Een interactieve virtual reality fietstocht tegen pijn en stress tijdens een behandeling</h1>
+      <p class="my-4">Een oplossing in samenwerking met Devign en SyncVR</p>
+      <a>Bekijk ons werk</a>
+    </div>
+  </div>
+</template>
+
+<style>
+.hero {
+  height: 32rem;
+  background-image: url('/backgrounds/hero-image.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center right;
+}
+</style>
+
+<script>
+export default {}
+</script>
