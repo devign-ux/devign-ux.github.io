a6684c0e02880df936adee68d841790ab61b0117
sven-zo <svenzo@outlook.com>
2020-01-15 22:00:18 +0100

    :sparkles: add header

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="header h-16 flex flex-row">
+    <div class="flex content-center flex-auto w-3/16 h-full">
+      <g-image src="~/images/devign-wordmark.svg" width="200" />
+    </div>
+    <div class="flex content-center header--links flex-initial self-end h-full">
+      <span>
+        <a>Proces</a>
+      </span>
+      <span>
+        <a>Prototype</a>
+      </span>
+      <span>
+        <a>Reflectie</a>
+      </span>
+    </div>
+    <div class="w-2/16"></div>
+  </header>
+</template>
+
+<style>
+.header .header--links span {
+  @apply px-8;
+  margin: auto;
+}
+
+.header .header--links span:last-child {
+  @apply px-4;
+}
+</style>
+
+<script>
+export default {}
+</script>
