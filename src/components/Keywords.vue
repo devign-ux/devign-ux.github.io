29df68a30c4492db4fdd7dbbce307a8d4c4e4c1b
sven-zo <svenzo@outlook.com>
2020-01-15 22:30:01 +0100

    :sparkles: add keywords

---
+++ src/components/Keywords.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="keywords bg-magenta-light h-20">
+    <div class="container mx-auto flex flex-row items-center justify-center h-full">
+      <span>Interactief</span>
+      <span>Gemoderniseerd</span>
+      <span>Behulpzaam</span>
+      <span>Innovatief</span>
+    </div>
+  </div>
+</template>
+
+<style>
+.keywords span {
+  @apply font-display;
+  @apply text-lg;
+  @apply font-medium;
+  @apply text-white;
+  @apply flex-auto;
+  @apply self-center;
+}
+</style>
+
+<script>
+export default {}
+</script>
