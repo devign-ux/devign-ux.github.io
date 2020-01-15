6f83c2a69c52f63c76e7f73ed156d1de55f0055d
sven-zo <svenzo@outlook.com>
2020-01-15 22:29:49 +0100

    :lipstick: improve hero style

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="hero">
-    <div class="h-full flex flex-col justify-center w-1/2">
+  <div class="hero flex">
+    <div class="w-2/16"></div>
+    <div class="h-full flex flex-col justify-center w-7/16">
       <h1>Een interactieve virtual reality fietstocht tegen pijn en stress tijdens een behandeling</h1>
       <p class="my-4">Een oplossing in samenwerking met Devign en SyncVR</p>
       <a>Bekijk ons werk</a>
@@ -12,7 +13,7 @@
 .hero {
   height: 32rem;
   background-image: url('/backgrounds/hero-image.png');
-  background-size: contain;
+  background-size: auto;
   background-repeat: no-repeat;
   background-position: center right;
 }
