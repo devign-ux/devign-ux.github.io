e71f04b5658a21cb130dd9343174651e3271f38f
sven-zo <svenzo@outlook.com>
2020-01-16 22:52:49 +0100

    :sparkles: improve methodcards

--- src/components/MethodCards.vue
+++ src/components/MethodCards.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="cards">
-    <div class="cards__block flex justify-center bg-grey-light">
-      <div class="cards__card bg-white rounded p-4 mr-4">
-        <h2 class="text-coal pb-4">Deskresearch</h2>
-        <p>Er is onderzoek gedaan naar de merkbeleving, de concurrentie, Technische specificaties van de camera, de doelgroep en de toepassing van design binnen VR.</p>
-      </div>
-      <div class="cards__card bg-white rounded p-4 mr-4">
-        <h2 class="text-coal pb-4">Fieldresearch</h2>
-        <p>Er zijn meerdere soorten onderzoeksmethoden toegepast, zoals observaties, interviews met de doelgroep, de opdrachtgever, superuser, customer journeys, persona’s. Ook zijn er verschillende instellingen van de camera getest.</p>
-      </div>
-      <div class="cards__card bg-white rounded p-4">
-        <h2 class="text-coal pb-4">User testing</h2>
-        <p>
-          Er zijn op meerdere momenten binnen het proces user testen en usability testen afgenomen met testpersonen buiten en binnen de doelgroep.
-          Hier is op geanalyseerd en geimplementeerd.
-        </p>
-      </div>
+    <div class="cards__block bg-grey-light">
+      <Box>
+        <div class="flex justify-around">
+          <div class="cards__card bg-white rounded p-4 mr-4">
+            <h2 class="text-coal pb-4">Deskresearch</h2>
+            <p>Er is onderzoek gedaan naar de merkbeleving, de concurrentie, Technische specificaties van de camera, de doelgroep en de toepassing van design binnen VR.</p>
+          </div>
+          <div class="cards__card bg-white rounded p-4 mr-4">
+            <h2 class="text-coal pb-4">Fieldresearch</h2>
+            <p>Er zijn meerdere soorten onderzoeksmethoden toegepast, zoals observaties, interviews met de doelgroep, de opdrachtgever, superuser, customer journeys, persona’s. Ook zijn er verschillende instellingen van de camera getest.</p>
+          </div>
+          <div class="cards__card bg-white rounded p-4">
+            <h2 class="text-coal pb-4">User testing</h2>
+            <p>
+              Er zijn op meerdere momenten binnen het proces user testen en usability testen afgenomen met testpersonen buiten en binnen de doelgroep.
+              Hier is op geanalyseerd en geimplementeerd.
+            </p>
+          </div>
+        </div>
+      </Box>
     </div>
   </div>
 </template>
@@ -39,5 +43,11 @@
 </style>
 
 <script>
-export default {}
+import Box from '~/components/Box.vue'
+
+export default {
+  components: {
+    Box
+  }
+}
 </script>
