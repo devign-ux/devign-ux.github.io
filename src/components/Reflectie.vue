835a30b23380cd8471dac56beb390005e2cd70b9
sven-zo <svenzo@outlook.com>
2020-01-16 23:53:23 +0100

    :sparkles: add hoofdstuk to reflectie

--- src/components/Reflectie.vue
+++ src/components/Reflectie.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="h-40 flex flex-col justify-center">
-    <Streepje />
-    <h1 class="text-center" id="reflectie">Reflectie</h1>
+  <div>
+    <div class="h-40 flex flex-col justify-center">
+      <Streepje />
+      <h1 class="text-center" id="reflectie">Reflectie</h1>
+    </div>
+    <Hoofdstuk :number="1" title="Reflectie" />
   </div>
-</template>
+</template> 
 
 <script>
 import Streepje from '~/components/Streepje.vue'
+import Hoofdstuk from '~/components/Hoofdstuk.vue'
 
 export default {
   components: {
-    Streepje
+    Streepje,
+    Hoofdstuk
   }
 }
 </script>
