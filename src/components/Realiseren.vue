e66eba3d3fb870f87f010f983bdd681f29dac434
sven-zo <svenzo@outlook.com>
2020-01-16 22:35:30 +0100

    :sparkles: improve realiseren

--- src/components/Realiseren.vue
+++ src/components/Realiseren.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="realiseren flex container ">
       <div class="w-2/16 p-16">
       </div>
@@ -25,10 +25,33 @@ Er zijn patiënten geinterviewd om tot meer inzichten te komen. Verpleegsters en
 <style>
 .realiseren {
   height: 24rem;
-}
+} 
+</style> -->
 
-</style>
+<template>
+  <Box class="flex my-16">
+    <div class="w-4/12">
+      <h1>Wat is er nodig om het project te realiseren?</h1>
+    </div>
+    <div class="w-2/12"></div>
+    <div class="w-6/12">
+      <p class="p-2">Een 360 graden camera, VirZOOM, fietstrainer en een standaard voor op de fiets.</p>
+      <p
+        class="p-2"
+      >Voor de beleving zijn filmpjes gemaakt bij bepaalde belangrijke bezienswaardigheden, zoals de stad Utrecht en Diergaarde Blijdorp. Deze zijn bewerkt in de daarvoor bedoelde software.</p>
+      <p
+        class="p-2"
+      >Er zijn patiënten geinterviewd om tot meer inzichten te komen. Verpleegsters en afdelingsmanagers zijn aanwezig geweest om toegang te geven op de afdelingen waar deze patiënten te vinden zijn.</p>
+    </div>
+  </Box>
+</template>
 
 <script>
-export default {}
+import Box from '~/components/Box'
+
+export default {
+  components: {
+    Box
+  }
+}
 </script>
