b6c1436968763baa584b5f5614f367b707f59518
sven-zo <svenzo@outlook.com>
2020-01-16 23:50:17 +0100

    :sparkles: add hoofdstuk to prototypes

--- src/components/Prototypes.vue
+++ src/components/Prototypes.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="h-40 flex flex-col justify-center">
-    <Streepje />
-    <h1 class="text-center" id="prototype">Prototype</h1>
+  <div>
+    <div class="h-40 flex flex-col justify-center">
+      <Streepje />
+      <h1 class="text-center" id="prototype">Prototype</h1>
+    </div>
+    <Hoofdstuk :number="1" title="Implementatie fase" />
   </div>
 </template>
 
 <script>
 import Streepje from '~/components/Streepje.vue'
+import Hoofdstuk from '~/components/Hoofdstuk.vue'
 
 export default {
   components: {
-    Streepje
+    Streepje,
+    Hoofdstuk
   }
 }
 </script>
