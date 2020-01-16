629eb48f2de91935d2383f17a5677c160d34008e
sven-zo <svenzo@outlook.com>
2020-01-16 21:27:32 +0100

    :fire: remove copy from index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,34 +3,11 @@
     <Header />
     <Hero />
     <Keywords />
-      <Belang />
-      <Doelen />
-      <Realiseren />
-      <Probleemstelling />
-    <Copy>
-      <h1>Stukje een</h1>
-      <p>
-        Voorbeeld stukje een. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error
-        doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur
-        rerum illum odit fugit assumenda rem dolores inventore iste
-        reprehenderit maxime! Iusto.
-      </p>
-      <h2>Stukje twee</h2>
-      <p>
-        Voorbeeld stukje twee. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error
-        doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur
-        rerum illum odit fugit assumenda rem dolores inventore iste
-        reprehenderit maxime! Iusto.
-      </p>
-      <h3>Stukje drie</h3>
-      <p>
-        Voorbeeld stukje drie. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error
-        doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur
-        rerum illum odit fugit assumenda rem dolores inventore iste
-        reprehenderit maxime! Iusto.
-      </p>
-      <SwitchableArea />
-    </Copy>
+    <Belang />
+    <Doelen />
+    <Realiseren />
+    <Probleemstelling />
+    <SwitchableArea />
   </Layout>
 </template>
 
@@ -50,7 +27,6 @@ import Belang from '~/components/Belang.vue'
 import Doelen from '~/components/Doelen.vue'
 import Realiseren from '~/components/Realiseren.vue'
 import Probleemstelling from '~/components/Probleemstelling.vue'
-
 
 export default {
   components: {
