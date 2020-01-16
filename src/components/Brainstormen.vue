49eff109d90d934bd9b45e124c697aa352949a2f
sven-zo <svenzo@outlook.com>
2020-01-16 22:21:10 +0100

    :sparkles: Add Box, Brainstormen

---
+++ src/components/Brainstormen.vue
@@ -0,0 +1,23 @@
+<template>
+  <Box class="flex mb-32">
+    <div class="w-6/12">
+      <g-image src="~/images/brainstormen.png" width="529" height="355"></g-image>
+    </div>
+    <div class="w-1/12"></div>
+    <div class="w-5/12">
+      <span class="text-magenta mb-4">Brainstormen</span>
+      <h1 class="mb-4">Het bedenken van drie verschillende concepten</h1>
+      <p>Om tot drie bruikbare concepten te komen zijn verschillende soorten brainstorm methoden gebruikt. Zo is er gebruik gemaakt van mindmapping, guidelines, feedback vanuit superuser en creative technique kaarten. Ook is er meerdere keren gefilmd om te bepalen welke instellingen gebruikt konden worden binnen het concept.</p>
+    </div>
+  </Box>
+</template>
+
+<script>
+import Box from '~/components/Box.vue'
+
+export default {
+  components: {
+    Box
+  }
+}
+</script>
