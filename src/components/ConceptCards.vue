cdb8ec6af92234efb93db28a77ee78b08a863e3c
sven-zo <svenzo@outlook.com>
2020-01-16 22:22:16 +0100

    :sparkles: add cards

---
+++ src/components/ConceptCards.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="concept-cards">
+    <div class="concept-cards__block flex justify-center bg-grey-light">
+      <div class="concept-cards__card bg-white rounded p-4 mr-4">
+        <h2 class="text-coal pb-4">Concept 1</h2>
+        <p>Via een speciaal keuzemenu, in de vorm van een ruimte (kamer), kan er worden gekeken naar de foto’s/schilderijen. Ook moet er een aantal seconden gefietst worden om de selectie te bevestigen. Op deze manier worden de trappers op een alternatieve interactieve manier ingezet. Tijdens de tocht worden geschiedenis beelden laten zien om het gevoel van nostalgie nog meer over te brengen.</p>
+      </div>
+      <div class="concept-cards__card bg-white rounded p-4 mr-4">
+        <h2 class="text-coal pb-4">Concept 2</h2>
+        <p>Het idee van dit concept was het maken van een Quiz aan het einde van de tocht. Om de antwoorden definitief te selecteren moest er een aantal seconden gefietst worden om zo weer extra interactie met de trappers te hebben. Ook was het idee dat door deze quiz mensen meer zouden opletten tijdens de tocht en zo nog meer afgeleid zouden zijn omdat ze de antwoord op de vragen zullen weten.</p>
+      </div>
+      <div class="concept-cards__card bg-white rounded p-4">
+        <h2 class="text-coal pb-4">Concept 3</h2>
+        <p>Tijdens de tocht wordt er stil gestaan op verschillende plekken. Tussentijds wordt er geteleporteerd naar een andere plek in de route. Er wordt informatie gegeven door audiofragmenten op de plekken waar bij wordt stilgestaan. Door het teleporteren tussen de stilstaande stukken door moet het bij de gebruiker ervoor zorgen dat ze minder misselijk worden van de bewegende beelden.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.concept-cards {
+  height: 30rem;
+}
+
+.concept-cards__block {
+  height: 20rem;
+}
+
+.concept-cards__card {
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  line-height: 1.3;
+  transform: translateY(5rem);
+  width: 350px;
+  height: 450px;
+}
+</style>
+
+<script>
+export default {}
+</script>
---
+++ src/components/MethodCards.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="cards">
+    <div class="cards__block flex justify-center bg-grey-light">
+      <div class="cards__card bg-white rounded p-4 mr-4">
+        <h2 class="text-coal pb-4">Deskresearch</h2>
+        <p>Er is onderzoek gedaan naar de merkbeleving, de concurrentie, Technische specificaties van de camera, de doelgroep en de toepassing van design binnen VR.</p>
+      </div>
+      <div class="cards__card bg-white rounded p-4 mr-4">
+        <h2 class="text-coal pb-4">Fieldresearch</h2>
+        <p>Er zijn meerdere soorten onderzoeksmethoden toegepast, zoals observaties, interviews met de doelgroep, de opdrachtgever, superuser, customer journeys, persona’s. Ook zijn er verschillende instellingen van de camera getest.</p>
+      </div>
+      <div class="cards__card bg-white rounded p-4">
+        <h2 class="text-coal pb-4">User testing</h2>
+        <p>
+          Er zijn op meerdere momenten binnen het proces user testen en usability testen afgenomen met testpersonen buiten en binnen de doelgroep.
+          Hier is op geanalyseerd en geimplementeerd.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.cards {
+  height: 30rem;
+}
+
+.cards__block {
+  height: 18rem;
+}
+
+.cards__card {
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  line-height: 1.3;
+  transform: translateY(5rem);
+  width: 350px;
+  height: 350px;
+}
+</style>
+
+<script>
+export default {}
+</script>
